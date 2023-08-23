3f42613435de24927eb20295b35b5158e9533358
dudagiglioli <dudaguilice@gmail.com>
2023-08-23 15:22:25 -0300

    fonts and images :construction:

--- compraApp/App.tsx
+++ compraApp/App.tsx
@@ -1,7 +1,23 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts,
+Montserrat_400Regular,
+Montserrat_400Regular_Italic,
+Montserrat_700Bold
+} from '@expo-google-fonts/montserrat';
 
 export default function App() {
+const [fonteCarregada] = useFonts({
+"MontserratRegular" : Montserrat_400Regular,
+"MontserratBold" : Montserrat_700Bold,
+"MontserratItalic" : Montserrat_400Regular_Italic
+})
+
+if (!fonteCarregada){
+  return null;
+}
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -16,5 +32,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontFamily: "MontserratItalic"
   },
 });
